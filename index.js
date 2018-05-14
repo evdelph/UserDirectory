0ba3f0390fe862a0c8c1c6c9244607052a81723a
Evelyn Marie Delph <evdelph@indiana.edu>
2018-05-14 16:46:42 -0400

    Homework done

--- index.js
+++ index.js
@@ -12,8 +12,33 @@ titleChange.textContent = myValue
 // Change text
 button.addEventListener('click',changeHeading)
 
+// Make new variable for refresh button
 const button2 = document.querySelector("#button2")
+
+// Refresh function
 function refreshPage(){
     location.reload()
 }
+
+// Click event
 button2.addEventListener('click',refreshPage)
+
+// Part 2 Code
+
+// Button 1_1
+const button1_1 = document.querySelector('#button1_1')
+function changeFirstSubHeading(){
+    let myValue = document.querySelector('input').value
+    let subtitleChange = document.querySelector('#first')
+    subtitleChange.textContent = myValue
+    }
+button1_1.addEventListener('click',changeFirstSubHeading)
+
+// Button 1_2
+const button1_2 = document.querySelector('#button1_2')
+function changeSecondSubHeading(){
+    let myValue = document.querySelector('input').value
+    let subtitleChange = document.querySelector('#second')
+    subtitleChange.textContent = myValue
+    }
+button1_2.addEventListener('click',changeSecondSubHeading)

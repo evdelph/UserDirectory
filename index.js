9b8c71fe95bb3601144933fd7f0aa2b47dfe8f36
Evelyn Marie Delph <evdelph@indiana.edu>
2018-05-15 16:52:14 -0400

    Work on CSS, except list output

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
+
 const form = document.querySelector('form#userForm')
-const userName = form.userName.value
-const age = form.age.value
-const favoriteColor = form.favoriteColor.value
 const users = document.querySelector('#users')
+
 const handleSubmit = function(ev) {
   ev.preventDefault()
   const form = ev.target
-//   const userName = form.userName.value
-//   const age = form.age.value
-//   const favoriteColor = form.favoriteColor.value
-//   const users = document.querySelector('#users')
-
-  //const list = document.createElement('ul')
-
-//   const nameItem = document.createElement('li')
-//   nameItem.textContent = `Name: ${userName}`
-
-//   const ageItem = document.createElement('li')
-//   ageItem.textContent = `Age: ${age}`
-
-//   const colorItem = document.createElement('li')
-//   colorItem.textContent = 'Favorite Color: '
-
-    
-  //colorItem.appendChild(renderColor())
-
-//   list.appendChild(nameItem)
-//   list.appendChild(ageItem)
-//   list.appendChild(colorItem)
-
   users.appendChild(renderList())
 
   form.reset()
@@ -37,6 +13,7 @@ const handleSubmit = function(ev) {
 
 function renderColor(){
   const colorDiv = document.createElement('div')
+  const favoriteColor = form.favoriteColor.value
   colorDiv.style.backgroundColor = favoriteColor
   colorDiv.style.width = '6rem'
   colorDiv.style.height = '3rem'
@@ -51,6 +28,9 @@ function renderListItem(item){
 
 function renderList(){
     const list = document.createElement('ul')
+    userName = form.userName.value
+    age = form.age.value
+
     const nameItem = renderListItem(`Name: ${userName}`)
     const ageItem = renderListItem(`Age: ${age}`)
     const colorItem = renderListItem('Favorite Color: ')
